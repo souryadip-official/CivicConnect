159405260e52889c6ed622a810d98acb621d21ee
Souryadip Mallick <souryadipmallick118@gmail.com>
2025-08-21 20:45:34 +0530

    Fixed production codes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 // backend/server.js
 
+const path = require('path'); 
 const express = require('express');
 const dotenv = require('dotenv');
-
-// FIXED: Load environment variables at the very top
-dotenv.config();
+dotenv.config(); 
 
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
@@ -14,21 +13,14 @@ const uploadRoutes = require('./routes/uploadRoutes');
 const userAdminRoutes = require('./routes/userAdminRoutes');
 const announcementRoutes = require('./routes/announcementRoutes');
 
-// Connect to the database
 connectDB();
 
 const app = express();
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
-// Use the routes
+// --- API Routes ---
+// All your API routes will go here
 app.use('/api/users', userRoutes);
 app.use('/api/organizations', organizationRoutes);
 app.use('/api/complaints', complaintRoutes);
@@ -36,6 +28,20 @@ app.use('/api/upload', uploadRoutes);
 app.use('/api/users/admin', userAdminRoutes);
 app.use('/api/announcements', announcementRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
